6a3b5230a539f8ff7c1b6668c98e82c05744231e
RKKon <rkkon@mail.ru>
2024-02-06 18:00:54 +0300

    Added accessibility to slider via keyboard.

--- js/parts/slider.js
+++ js/parts/slider.js
@@ -6,29 +6,44 @@ const Slider = () => {
     dotsWrap = document.querySelector(".slider-dots"),
     dots = document.querySelectorAll(".dot");
 
-  function showSlides(n) {
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    // slides.forEach((item) => item.style.display = 'none'); // newest and better way to do
+  const hideSlides = () => [...slides].map(slide => slide.style.display = 'none');
+
+  function showSlide(index) {
+    if (index > slides.length) slideIndex = 1
+    if (index < 1) slideIndex = slides.length
+    hideSlides();
     dots.forEach((item) => item.classList.remove("dot-active"));
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].classList.add("dot-active");
   }
-  showSlides(slideIndex);
+  showSlide(slideIndex);
+
+  const slideFocus = () => {
+    slides.forEach((slide, index) => {
+      slide.addEventListener('keydown', (e) => {
+        if (e.key === 'ArrowRight') {
+          slidesPlus(1);
+          setTimeout(() => slides[(index + 1) % slides.length].focus(), 0);
+        } else if (e.key === 'ArrowLeft') {
+          slidesPlus(-1);
+          setTimeout(() => slides[(index - 1 + slides.length) % slides.length].focus(), 0);
+        }
+      })
+
+      if (slide.classList.contains('slider-item')) {
+        slide.addEventListener('focus', () => slide.classList.add('slider_focus'))
+      }
+      slide.addEventListener('blur', () => slide.classList.remove('slider_focus'));
+    })
+  }
+  slideFocus();
 
   function slidesPlus(n) {
-    showSlides((slideIndex += n));
+    showSlide((slideIndex += n));
   }
 
   function currentSlide(n) {
-    showSlides((slideIndex = n));
+    showSlide((slideIndex = n));
   }
 
   prev.addEventListener("click", function () {
